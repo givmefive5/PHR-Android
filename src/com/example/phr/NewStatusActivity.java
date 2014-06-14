c963aa433397d7e83a24719d2223b2df0a36899f
ImTheMark <mark_serapion@yahoo.com>
2014-06-14 20:00:55 +0800

    UI Post Button

--- src/com/example/phr/NewStatusActivity.java
+++ src/com/example/phr/NewStatusActivity.java
@@ -4,6 +4,8 @@ import com.example.phr.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NewStatusActivity extends Activity {
 	
@@ -11,6 +13,24 @@ public class NewStatusActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_status);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_status_post, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+			case R.id.menu_item_status_post:
+				onBackPressed();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+		
 	}
 
 }
