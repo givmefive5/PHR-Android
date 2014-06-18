cdd4f14a1b5a18bd51ca2e6e316f578551815a51
cathy888 <yung.c2468@gmail.com>
2014-06-18 23:14:25 +0800

    edited

--- src/com/example/phr/SummaryReportFragment.java
+++ src/com/example/phr/SummaryReportFragment.java
@@ -11,10 +11,17 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import org.achartengine.ChartFactory;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 
 public class SummaryReportFragment extends Fragment {
 
@@ -57,9 +64,82 @@ public class SummaryReportFragment extends Fragment {
        	layout.addView(graphView); 
        	
        	
+       	//-------------------------------------------------------------------
+       	
+       	View mChart;
+        
+        String[] mMonth = new String[] {
+            "Jan", "Feb" , "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug" , "Sep", "Oct", "Nov", "Dec"
+        };
+        
+        int[] x = { 1,2,3,4,5,6,7,8 };
+        int[] income = { 2000,2500,2700,3000,2800,3500,3700,3800};
+        int[] expense = {2200, 2700, 2900, 2800, 2600, 3000, 3300, 3400 };
+ 
+        // Creating an  XYSeries for Income
+        XYSeries incomeSeries = new XYSeries("Income");
+        // Creating an  XYSeries for Expense
+        XYSeries expenseSeries = new XYSeries("Expense");
+        // Adding data to <span id="ygkmw72jk6f_10" class="ygkmw72jk6f">Income and Expense</span> Series
+        for(int i=0;i<x.length;i++){
+            incomeSeries.add(x[i], income[i]);
+            expenseSeries.add(x[i],expense[i]);
+        }
+ 
+        // Creating a dataset to hold each series
+        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+        // Adding Income Series to the dataset
+        dataset.addSeries(incomeSeries);
+        // Adding Expense Series to dataset
+        dataset.addSeries(expenseSeries);
+ 
+        // Creating XYSeriesRenderer to customize incomeSeries
+        XYSeriesRenderer incomeRenderer = new XYSeriesRenderer();
+        incomeRenderer.setColor(Color.WHITE);
+        incomeRenderer.setPointStyle(PointStyle.CIRCLE);
+        incomeRenderer.setFillPoints(true);
+        incomeRenderer.setLineWidth(2);
+        incomeRenderer.setDisplayChartValues(true);
+ 
+        // Creating XYSeriesRenderer to customize expenseSeries
+        XYSeriesRenderer expenseRenderer = new XYSeriesRenderer();
+        expenseRenderer.setColor(Color.YELLOW);
+        expenseRenderer.setPointStyle(PointStyle.CIRCLE);
+        expenseRenderer.setFillPoints(true);
+        expenseRenderer.setLineWidth(2);
+        expenseRenderer.setDisplayChartValues(true);
+ 
+        // Creating a XYMultipleSeriesRenderer to customize the whole chart
+        XYMultipleSeriesRenderer multiRenderer = new XYMultipleSeriesRenderer();
+        multiRenderer.setXLabels(0);
+        multiRenderer.setChartTitle("Income vs Expense Chart");
+        multiRenderer.setXTitle("Year 2012");
+        multiRenderer.setYTitle("Amount in Dollars");
+        multiRenderer.setZoomButtonsVisible(true);
+        for(int i=0;i<x.length;i++){
+            multiRenderer.addXTextLabel(i+1, mMonth[i]);
+        }
+ 
+        // Adding incomeRenderer and expenseRenderer to multipleRenderer
+        // Note: The order of adding dataseries to dataset and renderers to multipleRenderer
+        // should be same
+        multiRenderer.addSeriesRenderer(incomeRenderer);
+        multiRenderer.addSeriesRenderer(expenseRenderer);
+ 
+        // Getting a reference to LinearLayout of the MainActivity Layout
+        LinearLayout chartContainer = (LinearLayout) rootView.findViewById(R.id.linearLayoutWeight2);
+ 
+        // Creating a Line Chart
+        mChart = ChartFactory.getLineChartView(getActivity().getBaseContext(), dataset, multiRenderer);
+ 
+        // Adding the Line Chart to the LinearLayout
+        chartContainer.addView(mChart);
         
         
-		
 		return rootView;
 	}
+	
+	
+	
 }
