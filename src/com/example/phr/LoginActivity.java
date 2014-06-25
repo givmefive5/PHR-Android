4ad7352ac6350a68beb7f81f0f789dd7d1d63765
ImTheMark <mark_serapion@yahoo.com>
2014-06-25 21:14:20 +0800

    New files added

---
+++ src/com/example/phr/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.phr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import com.example.phr.R;
+
+public class LoginActivity extends Activity {
+	
+	private ImageButton mBtnLogin;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		mBtnLogin = (ImageButton) findViewById(R.id.btnLogin);
+		
+		mBtnLogin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+}
