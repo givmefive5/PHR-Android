68a72bbd76228c5d2cc03e12218ee390a44c4612
cathy888 <yung.c2468@gmail.com>
2014-06-25 15:03:46 +0800

    home

--- src/com/example/phr/SummaryReportFragment.java
+++ src/com/example/phr/SummaryReportFragment.java
@@ -1,7 +1,11 @@
 package com.example.phr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.phr.R;
 import android.graphics.Color;
+import android.graphics.Paint.Align;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,10 +16,14 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import org.achartengine.ChartFactory;
 import org.achartengine.chart.PointStyle;
+import org.achartengine.chart.BarChart.Type;
+import org.achartengine.model.CategorySeries;
 import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.SimpleSeriesRenderer;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
+
 
 public class SummaryReportFragment extends Fragment {
 
@@ -41,12 +49,44 @@ public class SummaryReportFragment extends Fragment {
 
 		// -------------------------------------------------------------------
 
-		cProgress = (ProgressBar) rootView.findViewById(R.id.progressBar2);
+		//cProgress = (ProgressBar) rootView.findViewById(R.id.progressBar2);
 		
-		cProgress.setProgress(cProgressStatus);
-		cProgress.setMax(1865);
+		//cProgress.setProgress(cProgressStatus);
+		//cProgress.setMax(1865);
+		
+		//---------------------------------------------------------------------
+		View dailyChart;
 		
 		
+		String[] titles = new String[] { "2008", "2007" };
+	    List<double[]> values = new ArrayList<double[]>();
+	    values.add(new double[] { 14230, 12300, 14240, 15244, 15900, 19200, 22030, 21200, 19500, 15500,
+	        12600, 14000 });
+	    values.add(new double[] { 5230, 7300, 9240, 10540, 7900, 9200, 12030, 11200, 9500, 10500,
+	        11600, 13500 });
+	    int[] colors = new int[] { Color.BLUE, Color.CYAN };
+	    XYMultipleSeriesRenderer renderer = buildBarRenderer(colors);
+	    setChartSettings(renderer, "Monthly sales in the last 2 years", "Month", "Units sold", 0.5,
+	        12.5, 0, 24000, Color.GRAY, Color.LTGRAY);
+	    renderer.getSeriesRendererAt(0).setDisplayChartValues(true);
+	    renderer.getSeriesRendererAt(1).setDisplayChartValues(true);
+	    renderer.setXLabels(12);
+	    renderer.setYLabels(10);
+	    renderer.setXLabelsAlign(Align.LEFT);
+	    renderer.setYLabelsAlign(Align.LEFT);
+	    renderer.setPanEnabled(true, false);
+	    renderer.setZoomEnabled(false);
+	    renderer.setZoomRate(1.1f);
+	    renderer.setBarSpacing(0.5f);
+		
+	    dailyChart = ChartFactory.getBarChartView(getActivity().getBaseContext(), buildBarDataset(titles, values), renderer,
+	            Type.STACKED);
+	    
+	    LinearLayout dailyContainer = (LinearLayout) rootView
+				.findViewById(R.id.linearLayoutWeight);
+	    
+	    dailyContainer.addView(dailyChart);
+	    
 		//-------------------------------------------------------------------
 		View mChart;
 
@@ -185,4 +225,51 @@ public class SummaryReportFragment extends Fragment {
 		return rootView;
 	}
 
+	
+
+	protected void setChartSettings(XYMultipleSeriesRenderer renderer,
+			String title, String xTitle, String yTitle, double xMin,
+			double xMax, double yMin, double yMax, int axesColor,
+			int labelsColor) {
+		renderer.setChartTitle(title);
+		renderer.setXTitle(xTitle);
+		renderer.setYTitle(yTitle);
+		renderer.setXAxisMin(xMin);
+		renderer.setXAxisMax(xMax);
+		renderer.setYAxisMin(yMin);
+		renderer.setYAxisMax(yMax);
+		renderer.setAxesColor(axesColor);
+		renderer.setLabelsColor(labelsColor);
+	}
+	 
+	 protected XYMultipleSeriesRenderer buildBarRenderer(int[] colors) {
+			XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+			renderer.setAxisTitleTextSize(16);
+			renderer.setChartTitleTextSize(20);
+			renderer.setLabelsTextSize(15);
+			renderer.setLegendTextSize(15);
+			int length = colors.length;
+			for (int i = 0; i <length; i++) {
+				SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+				r.setColor(colors[i]);
+				renderer.addSeriesRenderer(r);
+			}
+			return renderer;
+		}
+	 
+	 protected XYMultipleSeriesDataset buildBarDataset(String[] titles,
+				List<double[]> values) {//The data source and the pie chart almost, is by some key value pairs
+			XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+			int length = titles.length;
+			for (int i = 0; i <length; i++) {
+				CategorySeries series = new CategorySeries(titles[i]);
+				double[] v = values.get(i);
+				int seriesLength = v.length;
+				for (int k = 0; k <seriesLength; k++) {
+					series.add(v[k]);
+				}
+				dataset.addSeries(series.toXYSeries());
+			}
+			return dataset;
+		}
 }
