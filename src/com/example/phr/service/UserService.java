53c015b382c17659161e13981c10a993b9b74b10
givmefive5 <givme_five5@yahoo.com>
2014-06-25 22:20:22 +0800

    fixed register page

--- src/com/example/phr/service/UserService.java
+++ src/com/example/phr/service/UserService.java
@@ -2,12 +2,13 @@ package com.example.phr.service;
 
 import com.example.phr.exceptions.DuplicateUserException;
 import com.example.phr.exceptions.ServiceException;
+import com.example.phr.exceptions.UserAlreadyExistsException;
 import com.example.phr.model.User;
 
 public interface UserService {
 
 	public void registerUser(User user) throws DuplicateUserException,
-			ServiceException;
+			ServiceException, UserAlreadyExistsException;
 
 	public boolean validateUser(String username, String password)
 			throws ServiceException;
--- src/com/example/phr/serviceimpl/UserServiceImpl.java
+++ src/com/example/phr/serviceimpl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.phr.daoimpl.UserDaoImpl;
 import com.example.phr.exceptions.DatabaseErrorException;
 import com.example.phr.exceptions.DuplicateUserException;
 import com.example.phr.exceptions.ServiceException;
+import com.example.phr.exceptions.UserAlreadyExistsException;
 import com.example.phr.model.User;
 import com.example.phr.service.UserService;
 
@@ -14,7 +15,7 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public void registerUser(User user) throws DuplicateUserException,
-			ServiceException {
+			ServiceException, UserAlreadyExistsException {
 		try {
 			userDao.registerUser(user);
 		} catch (DatabaseErrorException e) {
