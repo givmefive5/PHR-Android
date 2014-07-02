b0bac6bcd17c42d3cc0e7e9499def2406b0bf74c
ImTheMark <mark_serapion@yahoo.com>
2014-07-02 22:35:39 +0800

    Food Tracker Layout

---
+++ src/com/example/phr/FoodTrackerPostActivity.java
@@ -0,0 +1,13 @@
+package com.example.phr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodTrackerPostActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_food_tracker_post);
+	}
+}
