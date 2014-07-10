89333b36aa7d80bc82a7884e2426ec1c525f7a0f
ImTheMark <mark_serapion@yahoo.com>
2014-07-10 21:26:22 +0800

    Activity Tracker Post implemented

---
+++ src/com/example/phr/ActivitiesTrackerPostActivity.java
@@ -0,0 +1,18 @@
+package com.example.phr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ActivitiesTrackerPostActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_activities_tracker_post);
+	}
+}
