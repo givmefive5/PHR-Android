3d8bf831948c7588b0cfb79e0244015de540feef
ImTheMark <mark_serapion@yahoo.com>
2014-08-15 16:01:36 +0800

    Register password check

--- src/com/example/phr/RegisterActivity.java
+++ src/com/example/phr/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.phr;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -22,6 +24,7 @@ public class RegisterActivity extends Activity {
 	private EditText formPassword;
 	private EditText formConfirmPassword;
 	private TextView mTextValid;
+	private TextView textViewPasswordStrength;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,38 +32,65 @@ public class RegisterActivity extends Activity {
 		setContentView(R.layout.activity_register);
 		mBtnRegister = (ImageButton) findViewById(R.id.btnRegister);
 		mTextValid =(TextView) findViewById(R.id.valid);
+		textViewPasswordStrength =(TextView) findViewById(R.id.textViewPasswordStrength);
+		
+		formUsername = (EditText) findViewById(R.id.txtUsernameReg);
+		formPassword = (EditText) findViewById(R.id.txtPasswordReg);
+		formConfirmPassword = (EditText) findViewById(R.id.confirmPasswordReg);
+		passwordValidator = new PasswordValidator();
+		formPassword.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				boolean valid = passwordValidator.validate(formPassword.getText().toString());
+				if(valid)
+					textViewPasswordStrength.setText("Password Strength: Strong");
+				else
+					textViewPasswordStrength.setText("Password Strength: Weak");
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+			}
+			
+		});
 		
 		
 		mBtnRegister.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				
-				formUsername = (EditText) findViewById(R.id.txtUsernameReg);
-				formPassword = (EditText) findViewById(R.id.txtPasswordReg);
-				formConfirmPassword = (EditText) findViewById(R.id.confirmPasswordReg);
-				
 				username = formUsername.getText().toString();
 				password = formPassword.getText().toString();
 				confirmPassword = formConfirmPassword.getText().toString();
 				
-				passwordValidator = new PasswordValidator();
+				
 				
 				boolean valid = passwordValidator.validate(password);
 				
-				if(valid)
+				if(password.equals(confirmPassword))
 				{
 					Log.e("tama1", "tama2");
-					if(password.equals(confirmPassword))
+					if(password.length()>7)
 					{
 						Intent intent = new Intent(getApplicationContext(), RegisterUserInformationActivity.class);
 						startActivity(intent);
 					}
 					else
-						mTextValid.setText("not same with confirm password");
+						mTextValid.setText("password length is not greater than 7");
 				}
 				else
 				{
-					mTextValid.setText("mali daw password");
+					mTextValid.setText("not same with confirm password");
 					Log.e("mali1", "mali2");
 				}
 				
