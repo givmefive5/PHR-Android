ab7029862bf71a471ee390a82a2296ee837ee9d2
ImTheMark <mark_serapion@yahoo.com>
2014-08-18 15:53:16 +0800

    SharedPreference

--- src/com/example/phr/LoginActivity.java
+++ src/com/example/phr/LoginActivity.java
@@ -3,14 +3,17 @@ package com.example.phr;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.phr.exceptions.ServiceException;
+import com.example.phr.local_db.SPreference;
 import com.example.phr.service.UserService;
 import com.example.phr.serviceimpl.UserServiceImpl;
 
@@ -43,16 +46,21 @@ public class LoginActivity extends Activity {
 		mBtnLogin = (ImageButton) findViewById(R.id.btnLogin);
 		mTextValid = (TextView) findViewById(R.id.checkLogin);
 
+		formUsername = (EditText) findViewById(R.id.txtUsername);
+		formPassword = (EditText) findViewById(R.id.txtPassword);
+		final SPreference sp = new SPreference(this.getApplicationContext());
+		
+		formUsername.setText(sp.loadPreferences("id"));
+
 		mBtnLogin.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 
-				formUsername = (EditText) findViewById(R.id.txtUsername);
-				formPassword = (EditText) findViewById(R.id.txtPassword);
-
 				username = formUsername.getText().toString();
 				password = formPassword.getText().toString();
-
+				
+				sp.savePreferences("id", username);
+/*
 				if (password.length() > 0 && username.length() > 0) {
 					try {
 						boolean isValid = userService.validateUser(username,
@@ -72,7 +80,7 @@ public class LoginActivity extends Activity {
 
 				else {
 					mTextValid.setText("Please Input Password & Username");
-				}
+				}*/
 
 			}
 		});
@@ -87,5 +95,48 @@ public class LoginActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+	}
+	
+	
+	
+	
+	/**
+	 *   Method used to get Shared Preferences */
+
+	public SharedPreferences getPreferences() 
+	{
+	    return getSharedPreferences("HealthGemPreferences", MODE_PRIVATE);
+	}
+	/**
+	 *  Method used to save Preferences */
+	public void savePreferences(String key, String value) 
+	{
+	    SharedPreferences sharedPreferences = getPreferences();
+	    SharedPreferences.Editor editor = sharedPreferences.edit();
+	    editor.putString(key, value);
+	    editor.commit();
+	}
+	/**
+	 *  Method used to load Preferences */
+	public String loadPreferences(String key) 
+	{
+	    try {
+	        SharedPreferences sharedPreferences = getPreferences();
+	        String strSavedMemo = sharedPreferences.getString(key, "");
+	        return strSavedMemo;
+	    } catch (NullPointerException nullPointerException) 
+	    {
+	        Log.e("Error caused at  TelaSketchUtin loadPreferences method",
+	                ">======>" + nullPointerException);
+	        return null;
+	    }
+	}
+	/**
+	 *  Method used to delete Preferences */
+	public boolean deletePreferences(String key)
+	{
+	    SharedPreferences.Editor editor=getPreferences().edit();
+	    editor.remove(key).commit();
+	    return false;
 	}
 }
